b508f6f2b0737be32d4cfb006f375ea78d40f20c
PiotrMa <piotrmgrupa4@gmail.com>
2017-07-17 15:51:17 +0200

    Stuck at perspective transform

--- src/util/visionUtils.java
+++ src/util/visionUtils.java
@@ -4,7 +4,12 @@ import java.awt.font.ImageGraphicAttribute;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -17,12 +22,17 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import com.sun.javafx.scene.traversal.Hueristic2D;
+
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.paint.ImagePattern;
 
 public class visionUtils {
 	
-	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+	static{
+		new LibraryLoader();
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		}
 	
 	static final ClassLoader loader = visionUtils.class.getClassLoader();
 	
@@ -35,8 +45,7 @@ public class visionUtils {
 	public void readImage() {
 		
 		//OPEN IMAGE
-		originalImg = Imgcodecs.imread("E:\\JAVA Projects\\OpenCv\\OCVImageSegmentation\\res\\shapes.jpg");
-		
+		originalImg = Imgcodecs.imread("D:\\workspace\\OCVImageSegmentation\\res\\sudoku.jpg");
 		//COPY AS A GRAYSCALE IMAGE FOR FURHTER PROCESSING
 		Mat gray = new Mat();
 		if(originalImg.empty()) {
@@ -48,14 +57,15 @@ public class visionUtils {
 				originalImg.copyTo(gray);
 			}
 			//CONTOURS
-			ContoursSegmentation(gray);
+			//contoursSegmentation(gray);
+			biggestContourSegmentation(gray);
 		}
 		
 		
 
 	}
 	
-	public void ContoursSegmentation(Mat img) {
+	public void contoursSegmentation(Mat img) {
 		Mat edges = new Mat();
 		//CANNY REDUCES A LOT OF NOISE SO FINDCONTOURS WILL BE MORE EFFECTIVE 
 		Imgproc.Canny(img, edges, 30, 200);
@@ -69,8 +79,148 @@ public class visionUtils {
 		System.out.println("NUMBER OF CONTOURS: "+contours.size());
 		
 		Imgproc.drawContours(originalImg, contours, -1, new Scalar(0,255,0),3);
-		Imgcodecs.imwrite("K:\\gaussian3.jpg", originalImg);
+		System.out.println("DONE");
+		Imgcodecs.imwrite("E:\\gaussian3.jpg", originalImg);
 	}
+	
+	public void biggestContourSegmentation(Mat img){
+		Mat edges = new Mat();
+		//CANNY REDUCES A LOT OF NOISE SO FINDCONTOURS WILL BE MORE EFFECTIVE 
+		Imgproc.Canny(img, edges, 30, 200);
+		unskewImage(edges);
+		//FINDCONTOURS
+		List<MatOfPoint> contours = new ArrayList<>();
+		Mat hierarch = new Mat();
+		
+		Imgproc.findContours(edges, contours, hierarch, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		
+		int maxArea = 0;
+		int maxIndex = 0;
+		
+		for (int i = 0; i < contours.size(); i++) {
+			if(Imgproc.contourArea(contours.get(i))>maxArea){
+				maxArea = (int)Imgproc.contourArea(contours.get(i));
+				maxIndex = i;
+			}
+		}
+		//Mat linesMatContour = new Mat().zeros(originalImg.size(), CvType.CV_8UC3);
+		//Imgproc.drawContours(linesMatContour, contours, maxIndex, new Scalar(0,255,0),5);
+		//Imgproc.cvtColor(linesMatContour, linesMatContour, Imgproc.COLOR_BGR2GRAY);
+		//Imgproc.threshold(linesMatContour, linesMatContour, 120, 255, Imgproc.THRESH_BINARY);
+		//Imgproc.Canny(linesMatContour, linesMatContour, 10, 50);
+		//Imgcodecs.imwrite("E:\\gaussian1.jpg", linesMatContour);
+		//unskewImage(linesMatContour);
+		
+		//sortingContours(contours);
+		System.out.println("DONE");
+		//Imgcodecs.imwrite("E:\\gaussian3.jpg", originalImg);
+		Imgcodecs.imwrite("E:\\gaussian3.jpg", cropContour(contours.get(maxIndex), originalImg));
+	}
+	
+	//PERSPECTIVE TRANSFORMATION - finding points
+	public void unskewImage(Mat img){
+		Mat copyImage = new Mat();
+		originalImg.copyTo(copyImage);
+		
+		//gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		//edges = cv2.Canny(gray,50,150,apertureSize = 3)
+		
+		Mat lines = new Mat();
+		//lines= cv2.HoughLines(dst, 1, math.pi/180.0, 100, np.array([]), 0, 0)
+		Imgproc.HoughLines(img, lines, 1, Math.PI/180.0, 100);
+		System.out.println(lines.dump());
+		double[] data;
+		double rho, theta;
+		Point pt1 = new Point();
+		Point pt2 = new Point();
+		double a, b;
+		double x0, y0;
+		for (int i = 0; i < lines.cols(); i++)
+		{
+		    rho = lines.get(i, 0)[0];
+		    theta = lines.get(i, 0)[1];
+		    a = Math.cos(theta);
+		    b = Math.sin(theta);
+		    x0 = a*rho;
+		    y0 = b*rho;
+		    pt1.x = Math.round(x0 + 1000*(-b));
+		    pt1.y = Math.round(y0 + 1000*a);
+		    pt2.x = Math.round(x0 - 1000*(-b));
+		    pt2.y = Math.round(y0 - 1000 *a);
+		    Imgproc.line(copyImage, pt1, pt2, new Scalar(0,255,0), 3);
+		    
+		}
+		Imgcodecs.imwrite("E:\\gaussian2.jpg", copyImage);
+	}
+	
+	//sudoku contour - first we would need to unskiew the image
+	public void sudokuSecondContour(Mat img){
+		Mat edges = new Mat();
+		//CANNY REDUCES A LOT OF NOISE SO FINDCONTOURS WILL BE MORE EFFECTIVE 
+		Imgproc.Canny(img, edges, 30, 200);
+		
+		//FINDCONTOURS
+		List<MatOfPoint> contours = new ArrayList<>();
+		Mat hierarch = new Mat();
+		
+		Imgproc.findContours(edges, contours, hierarch, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		
+/*		double minArea = 
+		
+		for (int i = 0; i < contours.size(); i++) {
+			if(Imgproc.contourArea(contours.get(i))>maxArea){
+				maxArea = (int)Imgproc.contourArea(contours.get(i));
+				maxIndex = i;
+			}
+		}*/
+		
+		Imgproc.drawContours(originalImg, contours, -1, new Scalar(0,255,0),3);
+		System.out.println("DONE");
+		Imgcodecs.imwrite("E:\\gaussian3.jpg", originalImg);
+	}
+	
+	//crop contour to a new image
+	public Mat cropContour(MatOfPoint contour, Mat img){
+		Rect croppedRect = Imgproc.boundingRect(contour);
+		return img.submat(croppedRect);
+	}
+	
+	//sorts downwards
+	public void sortingContours(List<MatOfPoint> contours){
+		List<MatOfPoint> sortedList = new ArrayList<>();
+		double maxArea = -1;
+		double currentArea = -1;
+		int insertToListIndex = -1;
+		int count = 0;
+		int maxCunt = contours.size();
+		while(maxCunt!=count){
+			if(count==0){
+				for (int i = 0; i < maxCunt; i++) {
+					if(Imgproc.contourArea(contours.get(i))>maxArea){
+						maxArea=Imgproc.contourArea(contours.get(i));
+						insertToListIndex=i;
+					}
+				}
+				
+			}else{
+				for (int i = 0; i < maxCunt; i++) {
+					if(Imgproc.contourArea(contours.get(i))>currentArea && Imgproc.contourArea(contours.get(i))<maxArea){
+						currentArea = Imgproc.contourArea(contours.get(i));
+						insertToListIndex=i;
+					}
+				}
+				maxArea=currentArea;
+				currentArea=-1;
+			}
+			sortedList.add(contours.get(insertToListIndex));
+			count++;
+		}
+		
+		for (int i = 0; i < sortedList.size(); i++) {
+			System.out.println(Imgproc.contourArea(sortedList.get(i)));
+		}
+	}
+
 	
 	public void edgeDetection(Mat img) {
 		Mat gray = new Mat();
@@ -102,7 +252,6 @@ public class visionUtils {
 			
 			Imgproc.rectangle(originalImg, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255));
 			System.out.println(rect);
-			Imgcodecs.imwrite("K:\\gaussian3.jpg", originalImg);
 		}else {
 			System.out.println("ERROR: I havent found any sudoku in the image");
 		}
